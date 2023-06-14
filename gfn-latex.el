11a3da77d4968698db2a80108fc80bce6c2eac5e
Takashi Suwa <bd.gfngfn@gmail.com>
2023-06-14 21:19:23 +0900

    update highlighting about '\divide' etc.

--- gfn-latex.el
+++ gfn-latex.el
@@ -91,11 +91,11 @@
      (1 'gfn-latex-ott-bracket-face t))
     ("\\(\\\\\\(?:expandafter\\|noexpand\\)\\)\\>"
      (1 'gfn-latex-expansion-control-face t))
-    ("\\(\\\\\\(?:global\\|let\\|futurelet\\|def\\|edef\\|gdef\\|xdef\\|newcount\\|newdimen\\|newbox\\|newskip\\|newtoks\\|newif\\|advance\\|multiply\\|setbox\\)\\)\\>"
+    ("\\(\\\\\\(?:global\\|let\\|futurelet\\|def\\|edef\\|gdef\\|xdef\\|newcount\\|newdimen\\|newbox\\|newskip\\|newtoks\\|newif\\|advance\\|multiply\\|divide\\|setbox\\)\\)\\>"
      (1 'gfn-latex-definition-face t))
     ("\\(\\\\\\(?:if[a-zA-Z@]*\\|else\\|or\\|fi\\)\\)\\>"
      (1 'gfn-latex-if-face t))
-    ("\\(\\\\\\(?:the\\|string\\|csname\\|endcsname\\)\\)\\>"
+    ("\\(\\\\\\(?:the\\|string\\|csname\\|endcsname\\|relax\\|box\\|wd\\|ht\\)\\)\\>"
      (1 'gfn-latex-important-primitive-face t))
     ("\\(\\\\\\(?:begin\\|end\\){\\)\\([a-zA-Z\\*]*\\)\\(}\\)"
      (1 'gfn-latex-environment-frame-face t)
